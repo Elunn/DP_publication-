8f432e2df03d608eeb36a652e5fccfff292bb2ca
Elunn <31861423+Elunn@users.noreply.github.com>
2020-06-06 17:39:48 +0100

    corrections

--- static/script/script.js
+++ static/script/script.js
@@ -3,15 +3,18 @@
 var aboutbtn = document.querySelector('#aboutBtn');
 
 aboutbtn.addEventListener('click', function(){
-    var about = document.querySelector('#about');
+    var about = document.querySelector('#about'),
+		main = document.querySelector("main");
 	
 
     if (about.classList == "dropDown") {
         about.classList.remove("dropDown");
 		aboutbtn.classList.add("white");
+		main.classList.add("hidden");
         
     } else {
 	   	about.classList.add("dropDown");
 		aboutbtn.classList.remove("white");
+		main.classList.remove("hidden");
     }
 });
